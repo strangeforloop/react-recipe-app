bfdcd01cc04ff29185a0933e6569a12fa3706cb4
strangeforloop <agohaa1@unlv.nevada.edu>
2020-01-19 15:46:05 -0800

    Updates

---
+++ precache-manifest.20d1f3b12c838d8188b3822f1f82f2e2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75d9e0cb9bc840d0fb7bf858ca467023",
+    "url": "/react-recipe-app/index.html"
+  },
+  {
+    "revision": "813130d53591d5be5be9",
+    "url": "/react-recipe-app/static/css/main.b66f23d2.chunk.css"
+  },
+  {
+    "revision": "7ce61e8b57f574870492",
+    "url": "/react-recipe-app/static/js/2.b26c0e45.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-recipe-app/static/js/2.b26c0e45.chunk.js.LICENSE"
+  },
+  {
+    "revision": "813130d53591d5be5be9",
+    "url": "/react-recipe-app/static/js/main.c71977b8.chunk.js"
+  },
+  {
+    "revision": "70807991e580235f0acc",
+    "url": "/react-recipe-app/static/js/runtime-main.c863515a.js"
+  },
+  {
+    "revision": "9538cb9857859556b7dbd8f66f909877",
+    "url": "/react-recipe-app/static/media/linuxLibertine.9538cb98.ttf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-recipe-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
